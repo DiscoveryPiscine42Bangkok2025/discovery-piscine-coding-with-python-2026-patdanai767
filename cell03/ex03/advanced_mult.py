87574bc1c97e179741d7576356b764d843307892
patdanai767 <pattproc@gmail.com>
2026-02-10 14:50:06 +0700

    refactor: replace for loop with while loop for multiplication table

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -7,8 +7,12 @@ mult = 0
 if len(sys.argv) > 1:
     print("none")
 else:
-    for i in range(mult, 11):
-        print(f"Table de {i}:",end="")
-        for j in range(0, 11):
-            print(f" {i*j}",end="")
+    while mult < 11:
+        print(f"Table de {mult}:",end="")
+        j = 0
+        while j < 11:
+            print(f" {mult*j}",end="")
+            j += 1
         print("")
+        mult += 1
+
